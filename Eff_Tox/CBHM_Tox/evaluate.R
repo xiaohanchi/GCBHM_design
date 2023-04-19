ba61ed322f30b22e797e798ab5d392718ed17df9
xiaohanchi <xiaohanchi123@gmail.com>
2023-04-18 22:20:50 -0500

    added file

---
+++ Eff_Tox/CBHM_Tox/evaluate.R
@@ -0,0 +1,25 @@
+# April 18, 2023
+# Code for manuscript: A Generalized Calibrated Bayesian Hierarchical Modeling Approach to Basket Trials with Multiple Endpoints
+# Code for evaluating the results from CBHM_Tox.R
+
+##load Rdata
+#setwd("~basket/simulation/eff&tox/n180rho0.3/CBHM_Tox/data")
+Ngroup<-4 # cancer groups/types
+nstage<-3 # trial stages(i.e.,#interim analyses=nstage-1)
+ngroup0<-c(15,15,15,15)
+nsimu<-5000
+### find c_eff
+load('var_1.Rdata')
+trial_final<-matrix(1, Ngroup,nsimu)*(n==(ngroup0[1]*nstage))
+c_eff<-sapply(1:Ngroup,function(r) quantile(pr_eff[r,]*trial_final[r,],.9))
+
+##calculate power in selected scenarios
+for(i in c(1,10,12,28,30,31,34,37,39)){
+  load(paste0('var_',i,'.Rdata'))
+  trial_final<-matrix(1, Ngroup,nsimu)*(n==(ngroup0[1]*nstage))
+  final_eff<-t(sapply(1:Ngroup,function(r) pr_eff[r,]>c_eff[r]))
+  final_eff<-final_eff*1*trial_final
+  power<-rowMeans(final_eff)
+  cat('probability of claiming efficacy by subgroup in scenario',i,':\n')
+  cat(formatC(c(power), digits=4, format="f"), sep ="  ", "\n") 
+}
