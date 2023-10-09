46bea9c8e01ff6f4e742d1506fe9397161d6b074
xiaohanchi <xiaohanchi123@gmail.com>
2023-10-09 16:05:07 -0500

    added functions

---
+++ Nested/GCBHM_m/functions.R
@@ -0,0 +1,44 @@
+##########################################################################################
+# Functions for Nested Efficacy Endpoints                                                #
+#                                                                                        #
+# Author: Xiaohan Chi                                                                    #
+# 10/09/2023                                                                             #
+##########################################################################################
+
+
+Tstat_fun<-function(y,n){
+  #input:
+  #y: vector with length of K*J, in a single trial
+  #n: vector with length of J, total samples of each groups in a single trial
+  E<-matrix(0,Ncategory,Ngroup)
+  y<-matrix(data = y,nrow = Ncategory,ncol = Ngroup,byrow = F)
+  sumy<-rowSums(y)
+  sumn<-sum(n)
+  E<-t(t(sumy))%*%n/sumn
+  if(0 %in% sumy){
+    E[which(sumy==0),]<-0.00001*n
+  }
+  Tstat<-sum((y-E)^2/E)
+  return(Tstat)
+}
+
+medianT_fun<-function(n,p){
+  #input:
+  #n: vector with length of J, total samples of each groups in a single trial
+  #p: KxJ matrix
+  #set.seed(2333)
+  y<-sapply(1:nsimu_c,function(r) sapply(1:Ngroup,function(r) rmultinom(rep(1,Ngroup),n,p[,r])))
+  Tstat<-sapply(1:nsimu_c,function(r) Tstat_fun(y[,r],n))
+  medianT<-median(Tstat)
+  return(medianT)
+}
+
+softmax_p<-function(p){
+  #p: vector with length K
+  p<-as.matrix(p)
+  result<-sapply(1:dim(p)[2],function(r) log(p[,r]/p[1,r]))
+  if(dim(p)[2]==1){
+    result<-as.vector(result)
+  }
+  return(result)
+}
