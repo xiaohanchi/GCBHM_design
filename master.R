dc1a51e8856775e8502cf9b5676d3fffc9846740
xiaohanchi <xiaohanchi123@gmail.com>
2023-12-30 19:36:52 -0600

    added master file

---
+++ master.R
@@ -0,0 +1,56 @@
+# Masterscript
+
+# Set GCBHM_design-main/ as working directory
+
+# Reproduce tables and figures in the manuscript
+# Reproduce tables and figures in the supplementary material
+
+source("Eff_Tox/BHM/evaluate.R")
+
+source("Eff_Tox/CBHM/evaluate.R")
+
+source("Eff_Tox/CBHM_Tox/evaluate.R")
+
+source("Eff_Tox/GCBHM_l/evaluate.R")
+
+source("Nested/BHM/evaluate.R")
+
+source("Nested/GCBHM_l/evaluate.R")
+
+source("Nested/GCBHM_m/evaluate.R")
+
+# Format tables as shown in manuscriptand supplementary material
+# Name and save ALL tables and figures in the folder results/
+
+
+
+# Rerun simulations 
+# Customize number of simulations to save running time, default 5000. Remove command lines defining nsimu in ALL related scripts.
+
+nsimu<-10
+
+
+
+
+
+# Run simulations # Calibrate where necessary
+source("Eff_Tox/BHM/BHM_HT.R")
+
+source("Eff_Tox/BHM/BHM_IG.R")
+
+source("Eff_Tox/CBHM/CBHM.R")
+
+source("Eff_Tox/CBHM_Tox/CBHM_Tox.R")
+
+source("Eff_Tox/GCBHM_l/calibration.R")
+source("Eff_Tox/GCBHM_l/GCBHM_l.R")
+
+source("Nested/BHM/BHM.R")
+
+source("Nested/GCBHM_l/calibration.R")
+source("Nested/GCBHM_l/GCBHM_l.R")
+
+source("Nested/GCBHM_m/calibration.R")
+source("Nested/GCBHM_m/GCBHM_m.R")
+
+
