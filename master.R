bf0b3c7be858eed8d860d251adf9ef74f8b4b1ce
xiaohanchi <xiaohanchi123@gmail.com>
2023-12-31 00:04:51 -0600

    updated BHM_HT code

--- master.R
+++ master.R
@@ -19,7 +19,7 @@ source("Nested/GCBHM_l/evaluate.R")
 
 source("Nested/GCBHM_m/evaluate.R")
 
-# Format tables as shown in manuscriptand supplementary material
+# Format tables as shown in manuscript and supplementary material
 # Name and save ALL tables and figures in the folder results/
 
 
@@ -34,7 +34,26 @@ nsimu<-10
 
 
 # Run simulations # Calibrate where necessary
+# try one script, run:
 source("Eff_Tox/BHM/BHM_HT.R")
+
+# run all scenarios:
+setwd("Eff_Tox/BHM")
+system("mv BHM_HT.R BHM_HT_1.R")
+system("sc_array=(1 10 12 37 39 28 30 31 34)") #selected scenarios shown in paper
+run_all<-"for i in ${sc_array[@]}
+do
+
+cp BHM_HT_1.R BHM_HT_$i.R
+find . -name 'BHM_HT_'$i'.R' -print0 | xargs -0 perl -pi -e 's/p\[,,1\]/p\[,,'$i'\]/g'
+find . -name 'BHM_HT_'$i'.R' -print0 | xargs -0 perl -pi -e 's/bhm_ht_1.Rdata/bhm_ht_'$i'.Rdata/g'
+Rscript BHM_HT_$i.R
+rm BHM_HT_$i.R
+
+done"
+system(run_all)
+system("mv BHM_HT_1.R BHM_HT.R")
+
 
 source("Eff_Tox/BHM/BHM_IG.R")
 
