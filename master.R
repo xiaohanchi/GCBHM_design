8329bea120f1899c69abe23dcf958679682fb719
xiaohanchi <xiaohanchi123@gmail.com>
2023-12-31 19:09:30 -0600

    added CBHM and CBHM_Tox in table 1 code

--- master.R
+++ master.R
@@ -3,10 +3,9 @@
 # Set GCBHM_design-main/ as working directory
 
 # Reproduce tables and figures in the manuscript
-# Reproduce tables and figures in the supplementary material
 
 ######################################################################################################
-#TABLE 1
+#TABLE 1 & 2
 source("Tables/Table1.R")
 
 source("Eff_Tox/CBHM/evaluate.R")
@@ -20,6 +19,8 @@ source("Nested/BHM/evaluate.R")
 source("Nested/GCBHM_l/evaluate.R")
 
 source("Nested/GCBHM_m/evaluate.R")
+
+# Reproduce tables and figures in the supplementary material
 
 # Format tables as shown in manuscript and supplementary material
 # Name and save ALL tables and figures in the folder results/
@@ -37,7 +38,10 @@ nsimu<-10
 
 
 # Run simulations # Calibrate where necessary
-############################################# BHM_IG #################################################
+
+############################################# TABLE 1 & 2 #################################################
+rho=0.3
+### BHM_IG
 # try one script, run:
 source("Eff_Tox/BHM/BHM_IG.R")
 
@@ -47,19 +51,20 @@ run_all<-"
 sc_array=(1 10 12 37 39 28 30 31 34)
 for i in ${sc_array[@]}
 do
-
-cp BHM_HT.R BHM_HT_$i.R
-find . -name 'BHM_IG_'$i'.R' -print0 | xargs -0 perl -pi -e \"s/p\\[,,1\\]/p\\[,,$i\\]/g\"
-find . -name 'BHM_IG_'$i'.R' -print0 | xargs -0 perl -pi -e 's/bhm_ig_1.Rdata/bhm_ig_'$i'.Rdata/g'
-
+  cp BHM_HT.R BHM_HT_$i.R
+  find . -name 'BHM_IG_'$i'.R' -print0 | xargs -0 perl -pi -e \"s/p\\[,,1\\]/p\\[,,$i\\]/g\"
+  find . -name 'BHM_IG_'$i'.R' -print0 | xargs -0 perl -pi -e 's/bhm_ig_1.Rdata/bhm_ig_'$i'.Rdata/g'
 done"
 system(run_all)
 for(i in c(1,10,12,37,39,28,30,31,34)){
   source(paste0("BHM_IG_",i,".R"))
 }
 system("rm BHM_IG_*")
-setwd("../")
-############################################# BHM_HT #################################################
+setwd("../../")
+
+
+
+### BHM_HT
 # try one script, run:
 source("Eff_Tox/BHM/BHM_HT.R")
 
@@ -69,25 +74,67 @@ run_all<-"
 sc_array=(1 10 12 37 39 28 30 31 34)
 for i in ${sc_array[@]}
 do
-
-cp BHM_HT.R BHM_HT_$i.R
-find . -name 'BHM_HT_'$i'.R' -print0 | xargs -0 perl -pi -e \"s/p\\[,,1\\]/p\\[,,$i\\]/g\"
-find . -name 'BHM_HT_'$i'.R' -print0 | xargs -0 perl -pi -e 's/bhm_ht_1.Rdata/bhm_ht_'$i'.Rdata/g'
-
+  cp BHM_HT.R BHM_HT_$i.R
+  find . -name 'BHM_HT_'$i'.R' -print0 | xargs -0 perl -pi -e \"s/p\\[,,1\\]/p\\[,,$i\\]/g\"
+  find . -name 'BHM_HT_'$i'.R' -print0 | xargs -0 perl -pi -e 's/bhm_ht_1.Rdata/bhm_ht_'$i'.Rdata/g'
 done"
 system(run_all)
 for(i in c(1,10,12,37,39,28,30,31,34)){
   source(paste0("BHM_HT_",i,".R"))
 }
 system("rm BHM_HT_*")
-setwd("../")
+setwd("../../")
 
 
-source("Eff_Tox/BHM/BHM_IG.R")
 
+
+### CBHM
+# try one script, run:
 source("Eff_Tox/CBHM/CBHM.R")
 
-source("Eff_Tox/CBHM_Tox/CBHM_Tox.R")
+# run all scenarios:
+setwd("Eff_Tox/CBHM")
+source("calibration.R") 
+run_all<-"
+sc_array=(1 10 12 37 39 28 30 31 34)
+for i in ${sc_array[@]}
+do
+  cp CBHM.R CBHM_$i.R
+  find . -name 'CBHM_'$i'.R' -print0 | xargs -0 perl -pi -e \"s/p\\[,,1\\]/p\\[,,$i\\]/g\"
+  find . -name 'CBHM_'$i'.R' -print0 | xargs -0 perl -pi -e 's/cbhm_1.Rdata/cbhm_'$i'.Rdata/g'
+done"
+system(run_all)
+for(i in c(1,10,12,37,39,28,30,31,34)){
+  source(paste0("CBHM_",i,".R"))
+}
+system("rm CBHM_*")
+setwd("../../")
+
+
+### CBHM_Tox
+setwd("Eff_Tox/CBHM_Tox")
+# try one script, run:
+source("CBHM_Tox.R")
+
+# run all scenarios:
+source("calibration.R") 
+run_all<-"
+sc_array=(1 10 12 37 39 28 30 31 34)
+for i in ${sc_array[@]}
+do
+  cp CBHM_Tox.R CBHM_Tox_$i.R
+  find . -name 'CBHM_Tox_'$i'.R' -print0 | xargs -0 perl -pi -e \"s/p\\[,,1\\]/p\\[,,$i\\]/g\"
+  find . -name 'CBHM_Tox_'$i'.R' -print0 | xargs -0 perl -pi -e 's/cbhmtox_1.Rdata/cbhmtox_'$i'.Rdata/g'
+done"
+system(run_all)
+for(i in c(1,10,12,37,39,28,30,31,34)){
+  source(paste0("CBHM_Tox_",i,".R"))
+}
+system("rm CBHM_Tox_*")
+setwd("../../")
+
+
+### GCBHM_l
 
 source("Eff_Tox/GCBHM_l/calibration.R")
 source("Eff_Tox/GCBHM_l/GCBHM_l.R")
